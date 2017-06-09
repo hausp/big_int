52c8d436d623a3ad34204ba2c1746cffddc56a7e
aszdrick <aszdrick@gmail.com>
2017-06-08 22:46:21 -0300

    Changed internal represenof number back to signal-magnitude and added multiplication operator

--- include/BigInt.hpp
+++ include/BigInt.hpp
@@ -28,15 +28,8 @@ namespace hausp {
         static constexpr auto GROUP_MAX = 0xffffffff;
         static constexpr auto GROUP_RADIX = 0x100000000;
         static constexpr auto GROUP_BIT_SIZE = 32;
-        static constexpr auto NEGATIVE = GROUP_MAX;
-        static constexpr auto POSITIVE = 0x0;
 
-        struct Signal {
-            Signal(bool);
-            Signal(Group);
-            bool negative() const;
-            Group value;
-        };
+        enum Signal : bool { POSITIVE = false, NEGATIVE = true };
      public:
         BigInt() = default;
         template<typename T, std::enable_if_t<std::is_unsigned<T>::value, int> = 0>
@@ -48,67 +41,49 @@ namespace hausp {
         BigInt& operator+=(const BigInt&);
         BigInt& operator-=(const BigInt&);
         BigInt operator-() const;
+        BigInt& operator*=(const BigInt&);
         BigInt& operator<<=(intmax_t);
         BigInt& operator>>=(intmax_t);
      private:
-        Signal signal = false;
+        Signal signal = Signal::POSITIVE;
         GroupVector data = {0};
 
-        void shrink();
-        void twoComplement();
-        BigInt toDecimal() const;
         template<typename Operation>
-        BigInt& carryOn(const BigInt&, Operation);
+        BigInt& carryOn(const BigInt&, bool, Operation);
+        BigInt& longMult(const BigInt&);
+        void shrink();
+
+        GroupVector toDecimal() const;
 
         static GroupVector convertBase(uintmax_t);
         static GroupVector convertBase(const std::string&);
+        static void twoComplement(GroupVector&, Group);
     };
 
-    inline BigInt::Signal::Signal(bool negative) {
-        negative ? value = NEGATIVE : value = POSITIVE;
-    }
-
-    inline BigInt::Signal::Signal(Group value) : value{value} { }
-
-    inline bool BigInt::Signal::negative() const {
-        return value == NEGATIVE;
-    }
-
     template<typename T, std::enable_if_t<std::is_unsigned<T>::value, int>>
-    BigInt::BigInt(T value): data{convertBase(value)} {
-        shrink();
-    }
+    BigInt::BigInt(T value): data{convertBase(value)} { }
 
     template<typename T, std::enable_if_t<std::is_signed<T>::value, int>>
-    BigInt::BigInt(T value): data{convertBase(std::abs(value))} {
-        if (value < 0) {
-            twoComplement();
-        }
-    }
+    BigInt::BigInt(T value):
+     signal{value < 0}, data{convertBase(std::abs(value))} { }
 
     inline BigInt BigInt::fromString(const std::string& str_value) {
         std::regex number_regex("\\s*(\\+|-)?\\s*([0-9]+)\\s*");
         std::smatch number_match;
-
         if (!std::regex_match(str_value, number_match, number_regex)) {
             throw std::runtime_error(
                 "Could not create BigInt from string: non-integer value"
             );
         }
-
         BigInt integer;
         integer.data = convertBase(number_match[2]);
-
-        if (number_match[1] == "-") {
-            integer.twoComplement();
-        }
-    
+        integer.signal = static_cast<Signal>(number_match[1] == "-");
+        integer.shrink();
         return integer;
     }
 
     inline BigInt::GroupVector BigInt::convertBase(uintmax_t value) {
         GroupVector data;
-
         if (value != 0) {
             while (value > 0) {
                 data.emplace_back(value & GROUP_MAX);
@@ -117,7 +92,6 @@ namespace hausp {
         } else {
             data.emplace_back(0);
         }
-
         return data;
     }
 
@@ -146,14 +120,8 @@ namespace hausp {
         return data;
     }
 
-    inline BigInt BigInt::toDecimal() const {
-        BigInt result = *this;
-        auto& dec_data = result.data;
-
-        if (signal.negative()) {
-            result.twoComplement();
-        }
-
+    inline BigInt::GroupVector BigInt::toDecimal() const {
+        auto dec_data = data;
         size_t k = 0;
         while (k < dec_data.size()) {
             for (size_t i = dec_data.size() - 1; i > k; --i) {
@@ -181,74 +149,35 @@ namespace hausp {
             dec_data.push_back(q);
         }
 
-        return result;
+        return dec_data;
     }
 
-    void BigInt::twoComplement() {
+    void BigInt::twoComplement(GroupVector& data, Group signal) {
         DoubleGroup carry = 1;
         for (auto& segment : data) {
             DoubleGroup complement = ~segment + carry;
             segment = complement;
             carry = complement >> GROUP_BIT_SIZE;
         }
-        signal.value = ~signal.value + carry;
-        if (signal.value == 1) {
-            data.emplace_back(signal.value);
-            signal.value = 0;
+        signal = ~signal + carry;
+        if (signal == 1) {
+            data.emplace_back(signal);
+            signal = 0;
         }
-        shrink();
     }
 
     inline void BigInt::shrink() {
-        while (data.back() == signal.value && data.size() > 1) {
+        while (data.back() == 0 && data.size() > 1) {
             data.pop_back();
         }
     }
 
-    inline bool operator==(const BigInt& lhs, const BigInt& rhs) {
-        if (lhs.signal.value != rhs.signal.value) {
-            return false;
-        } else if (lhs.data.size() == rhs.data.size()) {
-            size_t i = lhs.data.size() - 1;
-            while (i > 0 && lhs.data[i] == rhs.data[i]) --i;
-            return i == 0 && lhs.data[i] == rhs.data[i];
-        }
-        return false;
-    }
-
-    inline bool operator!=(const BigInt& lhs, const BigInt rhs) {
-        return !(lhs == rhs);
-    }
-
-    inline bool operator<(const BigInt& lhs, const BigInt& rhs) {
-        if (lhs.signal.value != rhs.signal.value) {
-            return lhs.signal.negative();
-        } else if (lhs.data.size() == rhs.data.size()) {
-            size_t i = lhs.data.size() - 1;
-            while (i > 0 && lhs.data[i] == rhs.data[i]) --i;
-            return lhs.data[i] < rhs.data[i];
-        }
-        return lhs.data.size() < rhs.data.size();
-    }
-
-    inline bool operator>(const BigInt& lhs, const BigInt& rhs) {
-        return rhs < lhs;
-    }
-
-    inline bool operator<=(const BigInt& lhs, const BigInt& rhs) {
-        return !(lhs > rhs);
-    }
-
-    inline bool operator>=(const BigInt& lhs, const BigInt& rhs) {
-        return !(lhs < rhs);
-    }
-
     template<typename Operation>
-    BigInt& BigInt::carryOn(const BigInt& rhs, Operation op) {
+    BigInt& BigInt::carryOn(const BigInt& rhs, bool cmpl, Operation op) {
         if (data.size() < rhs.data.size()) {
-            data.insert(data.end(), rhs.data.size(), signal.value);
+            data.insert(data.end(), rhs.data.size() - data.size(), 0);
         }
-        DoubleGroup carry = 0; // Uses DoubleGroup to coerce operation
+        DoubleGroup carry = cmpl; // Uses DoubleGroup to coerce operation
         size_t i = 0;
         for (i = 0; i < rhs.data.size(); ++i) {
             DoubleGroup result = op(data[i], rhs.data[i], carry);
@@ -256,36 +185,74 @@ namespace hausp {
             carry = (result >> GROUP_BIT_SIZE) > 0;
         }
         while (i < data.size() && carry != 0) {
-            DoubleGroup result = op(data[i], rhs.signal.value, carry);
+            DoubleGroup result = op(data[i], 0, carry);
             data[i] = result;
             carry = (result >> GROUP_BIT_SIZE) > 0;
             ++i;
         }
-        auto signal_update = op(signal.value, rhs.signal.value, carry);
-        auto carry_out = (signal_update >> GROUP_BIT_SIZE) > 0;
-        signal.value = signal_update;
-        if (carry_out ^ carry) {
-            data.emplace_back(carry);
+
+        if (cmpl) {
+            auto signal_update = op(0, 0, carry);
+            auto carry_out = (signal_update >> GROUP_BIT_SIZE) > 0;
+            Group value = signal_update;
+            if (carry_out ^ carry) {
+                data.emplace_back(carry);
+            }
+            if (value == GROUP_MAX) {
+                twoComplement(data, value);
+                signal = NEGATIVE;
+            } else if (value == 0) {
+                signal = POSITIVE;
+            }
+        } else if (carry > 0) {
+            data.push_back(carry);
         }
         shrink(); // Is it worth?
         return *this;
     }
 
     inline BigInt& BigInt::operator+=(const BigInt& rhs) {
-        return carryOn(rhs, [](Group lhs, Group rhs, DoubleGroup carry) {
-            return carry + lhs + rhs;
-        });
+        if (signal == rhs.signal) {
+            return carryOn(rhs, false,
+                [](Group lhs, Group rhs, DoubleGroup carry) {
+                    return carry + lhs + rhs;
+                }
+            );
+        } else if (signal && !rhs.signal) {
+            return carryOn(rhs, true,
+                [](Group lhs, Group rhs, DoubleGroup carry) {
+                    return carry + ~lhs + rhs;
+                }
+            );
+        } else {
+            return carryOn(rhs, true,
+                [](Group lhs, Group rhs, DoubleGroup carry) {
+                    auto result = carry + lhs + ~rhs;
+                    return result;
+                }
+            );
+        }
     }
 
     inline BigInt& BigInt::operator-=(const BigInt& rhs) {
-        return carryOn(rhs, [](Group lhs, Group rhs, DoubleGroup carry) {
-            return -carry + lhs - rhs;
-        });
+        if (signal == rhs.signal) {
+            return carryOn(rhs, true,
+                [](Group lhs, Group rhs, DoubleGroup carry) {
+                    return carry + lhs + ~rhs;
+                }
+            );
+        } else {
+            return carryOn(rhs, false,
+                [](Group lhs, Group rhs, DoubleGroup carry) {
+                    return carry + lhs + rhs;
+                }
+            );
+        }
     }
 
     inline BigInt BigInt::operator-() const {
         BigInt result = *this;
-        result.twoComplement();
+        result.signal = static_cast<Signal>(!signal);
         return result;
     }
 
@@ -297,6 +264,33 @@ namespace hausp {
     inline BigInt operator-(const BigInt& lhs, const BigInt& rhs) {
         auto copy = lhs;
         return copy -= rhs;
+    }
+
+    BigInt& BigInt::longMult(const BigInt& rhs) {
+        auto product = GroupVector(data.size() + rhs.data.size() + 1, 0);
+        for (size_t i = 0; i < rhs.data.size(); ++i) {
+            DoubleGroup carry = 0;
+            for (size_t j = 0; j < data.size(); ++j) {
+                DoubleGroup result = rhs.data[i];
+                result = product[i + j] + result * data[j] + carry;
+                product[i + j] = result;
+                carry = result >> GROUP_BIT_SIZE;
+            }
+            product[data.size() + i] += carry;
+        }
+        signal = static_cast<Signal>(signal != rhs.signal);
+        data = product;
+        shrink();
+        return *this;
+    }
+
+    inline BigInt& BigInt::operator*=(const BigInt& rhs) {
+        return longMult(rhs);
+    }
+
+    inline BigInt operator*(const BigInt& lhs, const BigInt& rhs) {
+        auto copy = lhs;
+        return copy *= rhs;
     }
 
     inline BigInt& BigInt::operator<<=(intmax_t shift) {
@@ -317,15 +311,7 @@ namespace hausp {
         }
 
         if (carried_bits > 0) {
-            if (signal.negative()) {
-                auto mask = (1 << (shift - 1)) - 1;
-                auto extension = carried_bits ^ mask;
-                if (extension) {
-                    data.emplace_back(carried_bits | ~extension);
-                }
-            } else {
-                data.emplace_back(carried_bits);
-            }
+            data.emplace_back(carried_bits);
         }
 
         shrink(); // Is it worth?
@@ -343,7 +329,7 @@ namespace hausp {
 
         if (group_shift > data.size()) {
             data.clear();
-            data.emplace_back(signal.value);
+            data.emplace_back(signal);
             return *this;
         }
 
@@ -351,7 +337,8 @@ namespace hausp {
             data.pop_front();
         }
 
-        Group carried_bits = signal.value << (GROUP_BIT_SIZE - shift);
+        // Group carried_bits = signal.value << (GROUP_BIT_SIZE - shift);
+        Group carried_bits = 0;
         for (intmax_t i = data.size() - 1; i >= 0; --i) {
             auto shifted_bits = data[i] & shift_mask;
             data[i] = (data[i] >> shift) | carried_bits;
@@ -359,6 +346,11 @@ namespace hausp {
         }
 
         shrink(); // Is it worth?
+
+        if (signal && data.size() == 1 && data.back() == 0) {
+            data.back() = 1;
+        }
+
         return *this;
     };
 
@@ -372,15 +364,52 @@ namespace hausp {
         return result >>= rhs;
     }
 
+    inline bool operator==(const BigInt& lhs, const BigInt& rhs) {
+        if (lhs.signal != rhs.signal) {
+            return false;
+        } else if (lhs.data.size() == rhs.data.size()) {
+            size_t i = lhs.data.size() - 1;
+            while (i > 0 && lhs.data[i] == rhs.data[i]) --i;
+            return i == 0 && lhs.data[i] == rhs.data[i];
+        }
+        return false;
+    }
+
+    inline bool operator!=(const BigInt& lhs, const BigInt rhs) {
+        return !(lhs == rhs);
+    }
+
+    inline bool operator<(const BigInt& lhs, const BigInt& rhs) {
+        if (lhs.signal != rhs.signal) {
+            return lhs.signal;
+        } else if (lhs.data.size() == rhs.data.size()) {
+            size_t i = lhs.data.size() - 1;
+            while (i > 0 && lhs.data[i] == rhs.data[i]) --i;
+            return lhs.data[i] < rhs.data[i];
+        }
+        return lhs.data.size() < rhs.data.size();
+    }
+
+    inline bool operator>(const BigInt& lhs, const BigInt& rhs) {
+        return rhs < lhs;
+    }
+
+    inline bool operator<=(const BigInt& lhs, const BigInt& rhs) {
+        return !(lhs > rhs);
+    }
+
+    inline bool operator>=(const BigInt& lhs, const BigInt& rhs) {
+        return !(lhs < rhs);
+    }
+
     template<typename... Args>
     BigInt stobi(Args&&... args) {
         return BigInt::fromString(std::forward<Args>(args)...);
     }
 
     inline std::ostream& operator<<(std::ostream& out, const BigInt& number) {
-        BigInt decimal = number.toDecimal();
-        auto dec_data = decimal.data;
-        if (number.signal.negative()) out << "-";
+        auto dec_data = number.toDecimal();
+        if (number.signal == BigInt::Signal::NEGATIVE) out << "-";
         auto last = dec_data.size() - 1;
         for (size_t i = last + 1; i > 0; --i) {
             auto index = i - 1;
